f3ae1b50a9dc85674f76c7d931a8d707010323ff
Akshay Gehlot <37250279+Akshay-SG@users.noreply.github.com>
2020-03-17 19:34:14 -0400

    Updated solution (without test cases)

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
+### Akshay S Gehlot's solution for Kargo Internship test 
+### March 2020
+
+
 import sys
 
-max_len = 26  
-def string_map(string1, string2):  
-    l1 = len(string1) 
-    l2 = len(string2)
-    if (l1 != l2): 
-        return False
-    freq_s1 = [0 for i in range(max_len)] 
-    freq_s2 = [0 for i in range(max_len)] 
-    for i in range(l1): 
-        freq_s1[ord(string1[i]) - ord('a')] += 1
-    for i in range(l2): 
-        freq_s2[ord(string2[i]) - ord('a')] += 1
-    for i in range(max_len):  
-        if (freq_s1[i] == 0): 
-            continue
-        found = False
-        for j in range(max_len): 
-            if (freq_s1[i] == freq_s2[j]): 
-                freq_s2[j] = -1 
-                found = True
-                break 
-        if (found==False): 
-            return False
-    return True
-
-if __name__ == '__main__': 
-    s1 = sys.argv[1]
-    s2 = sys.argv[2] 
-    print(string_map(s1, s2))
+def one_to_one(s1,s2):
+    if len(s1)<=len(s2):
+        d={}
+        for k in range (len(s1)):
+            if s1[k] not in d:
+                d[s1[k]]=s2[k]
+                
+            if d[s1[k]]!=s2[k]:
+                return False
+        return True
+    return False 
